51c4f23beb74ccfea41dc696413a88ca1a1c5913
agus24ti <agustian24ti@mahasiswa.pcr.ac.id>
2026-01-14 16:40:30 +0700

    agus1

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('dashboard', function () {
-    return view('admin.dashboard');
-});
 Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 Route::get('/admin/forms', [AdminController::class, 'forms'])->name('admin.forms');
 Route::get('/admin/tables', [AdminController::class, 'tables'])->name('admin.tables');
